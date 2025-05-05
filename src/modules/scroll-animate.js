622232ed2b8a76a215abe2dc0f39fa13d14b3532
FehPereira <fehpereira237@gmail.com>
2025-05-05 16:24:52 -0300

    criando uma função para recuperar a distancia entre o topo da página e o topo do elemento

--- src/modules/scroll-animate.js
+++ src/modules/scroll-animate.js
@@ -1,6 +1,13 @@
 export class ScrollSmooth {
-  constructor(sections, windowHalf){
-    this.sections = document.querySelectorAll(sections)
-    this.windowHalf = windowHalf
+  constructor(sections, windowHalf) {
+    this.sections = document.querySelectorAll(sections);
+    this.windowHalf = windowHalf;
+    this.topOfElements();
+  }
+
+  topOfElements() {
+    this.sections.forEach((section) => {
+      const distanceOfTop = section.offsetTop
+    });
   }
 }
